be842338522ee8fa7778dbfaec9486524a006679
Eric Klaus <34795919+emklaus@users.noreply.github.com>
2019-11-08 12:31:55 -0500

    Add files via upload
    STM32F103_UARTx - the basic USART implementation (simple polling - no interrupts)

---
+++ STM32F103_UARTx/Debug/CMSIS/core/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../CMSIS/core/core_cm3.c 
+
+OBJS += \
+./CMSIS/core/core_cm3.o 
+
+C_DEPS += \
+./CMSIS/core/core_cm3.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+CMSIS/core/%.o: ../CMSIS/core/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -DSTM32F1 -DSTM32F103C8Tx -DSTM32 -DUSE_STDPERIPH_DRIVER -DSTM32F10X_MD -DDEBUG -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/inc" -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/CMSIS/core" -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/StdPeriph_Driver/inc" -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/CMSIS/device" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ STM32F103_UARTx/Debug/startup/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_UPPER_SRCS += \
+../startup/startup_stm32f10x_md.S 
+
+OBJS += \
+./startup/startup_stm32f10x_md.o 
+
+S_UPPER_DEPS += \
+./startup/startup_stm32f10x_md.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+startup/%.o: ../startup/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -x assembler-with-cpp -DSTM32F1 -DSTM32F103C8Tx -DSTM32 -DUSE_STDPERIPH_DRIVER -DSTM32F10X_MD -DDEBUG -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/inc" -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/StdPeriph_Driver/inc" -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/CMSIS/device" -I"C:/STM32/Eclipse_Projects/STM32F103_UARTx/CMSIS/core" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
